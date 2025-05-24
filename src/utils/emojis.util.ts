4c42d4f0c0c5debf4ee90d80a1e117625ed11826
wong-zq <zq@securemyscholarship.com>
2025-05-25 01:11:53 +0800

    feat: update logic to use emoji text without trailing >

--- src/utils/emojis.util.ts
+++ src/utils/emojis.util.ts
@@ -1,21 +1,21 @@
 import { EMimeType, convertImageURLtoURI } from './image.util';
 
 export const parseEmoji = async (content: string) => {
+  console.log('content', content);
+
   // animated emoji content <a:pepedistraught:1238422605782847599>
-  const animatedEmojiRegex = /<a:[A-Za-z0-9_]+:[0-9]+>/;
+  const animatedEmojiRegex = /^<a:[A-Za-z0-9_]+:[0-9]+>$/;
   // static emoji content <:pepedistraught:1238422605782847599>
-  const staticEmojiRegex = /<:[A-Za-z0-9_]+:[0-9]+>/;
-  // animated emoji link content https://cdn.discordapp.com/emojis/1285818661902221344.webp?size=96&animated=true
-  const animatedEmojiLinkRegex =
-    /^https:\/\/cdn\.discordapp\.com\/emojis\/[0-9]+\.webp\?size=[0-9]+&animated=true [A-Za-z0-9_]+$/;
-  // static emoji link content https://cdn.discordapp.com/emojis/1235181441957105674.webp?size=96
-  const staticEmojiLinkRegex =
-    /^https:\/\/cdn\.discordapp\.com\/emojis\/[0-9]+\.webp\?size=[0-9]+ [A-Za-z0-9_]+$/;
+  const staticEmojiRegex = /^<:[A-Za-z0-9_]+:[0-9]+>$/;
+  // animated emoji content <a:pepedistraught:1238422605782847599
+  const animatedEmojiNoServerRegex = /^<a:[A-Za-z0-9_]+:[0-9]+$/;
+  // static emoji content <:pepedistraught:1238422605782847599
+  const staticEmojiNoServerRegex = /^<:[A-Za-z0-9_]+:[0-9]+$/;
 
   const isAnimatedEmoji = animatedEmojiRegex.test(content);
   const isStaticEmoji = staticEmojiRegex.test(content);
-  const isAnimatedEmojiLink = animatedEmojiLinkRegex.test(content);
-  const isStaticEmojiLink = staticEmojiLinkRegex.test(content);
+  const isAnimatedEmojiNoServer = animatedEmojiNoServerRegex.test(content);
+  const isStaticEmojiNoServer = staticEmojiNoServerRegex.test(content);
 
   let emojiName: string | null = null;
   let emojiURI: string | null = null;
@@ -40,10 +40,26 @@ export const parseEmoji = async (content: string) => {
 
     const pngURL = `https://cdn.discordapp.com/emojis/${_emojiId}.png`;
     emojiURI = await convertImageURLtoURI(pngURL, EMimeType.PNG);
-  } else if (isAnimatedEmojiLink || isStaticEmojiLink) {
-    const [_emojiURI, _emojiName] = content.split(' ');
-    emojiName = _emojiName;
-    emojiURI = _emojiURI;
+  } else if (isAnimatedEmojiNoServer) {
+    // remove leading '<a:'
+    const [_emojiName, _emojiId] = content
+      .substring(3, content.length)
+      .split(':');
+
+    emojiName = `uu_${_emojiName}`;
+
+    const gifURL = `https://cdn.discordapp.com/emojis/${_emojiId}.gif`;
+    emojiURI = await convertImageURLtoURI(gifURL, EMimeType.PNG);
+  } else if (isStaticEmojiNoServer) {
+    // remove leading '<:'
+    const [_emojiName, _emojiId] = content
+      .substring(2, content.length)
+      .split(':');
+
+    emojiName = `uu_${_emojiName}`;
+
+    const pngURL = `https://cdn.discordapp.com/emojis/${_emojiId}.png`;
+    emojiURI = await convertImageURLtoURI(pngURL, EMimeType.PNG);
   }
 
   if (emojiName && emojiURI) {
